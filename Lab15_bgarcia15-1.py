2a47e8cf070b7a6982fd6c5d7419ed9389ccfb3e
21garcbe <21garcbe@gmail.com>
2026-05-03 21:45:29 -0400

    Initial commit

---
+++ Lab15_bgarcia15-1.py
@@ -0,0 +1,30 @@
+"""Lab 15: plotting a formula with matplotlib
+Author: Ben Garcia
+
+program uses matplotlib to plot an Archimedian Spiral
+
+The formula for an Archimedian Spiral is r = a + b * theta
+where r is the radius, theta is the angle, and a and b are constants that determine the shape of the spiral. 
+In this program, we will use a = 0 and b = 0.05 to create a simple spiral.
+
+"""
+import matplotlib.pyplot as plt
+import math
+
+def main():
+    """Main function to plot an Archimedian spiral."""
+    n = 100
+
+    x_values = []
+    y_values = []
+
+
+   
+
+
+def spiral(t):
+    """Returns an x, y point on an Archimedian spiral."""
+    
+
+if __name__ == "__main__":
+    main()
